a7d6d11bd2d4e3a80240fc1ac8be4e3b21f4f5f6
user42 <ander_alvarenga@hotmail.com]>
2021-02-25 14:17:11 +0100

    using open

--- main.c
+++ main.c
@@ -6,14 +6,33 @@
 /*   By: aalcara- <aalcara-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/25 00:31:25 by aalcara-          #+#    #+#             */
-/*   Updated: 2021/02/25 02:00:17 by aalcara-         ###   ########.fr       */
+/*   Updated: 2021/02/25 14:16:00 by aalcara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line/get_next_line.h"
+#include <sys/types.h> //open
+#include <sys/stat.h> //open
+#include <fcntl.h> //open
+#include <stdio.h> //printf
+/*	OPEN
+
+	int open(const char *pathname, int flags);
+		O_RDONLY: flag to read only
+		
+		return: return the fd used or -1 on error.
+		
+
+*/
 
 int				main(void)
 {
-	get_next_line();
+	int		fd1;
+
+	fd1 = open("teste.txt", O_RDONLY);
+	printf("teste, fd1 = %d", fd1);
+	
+
+	
 	
 }
