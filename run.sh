9a4ca72cf32e476ffdf66b3a5973b786a683fa07
Anderson-ubuntu <ander_alvarenga@hotmail.com>
2021-03-08 14:20:34 -0300

    finish

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #gcc -Wall -Wextra -Werror -D BUFFER_SIZE=10 main.c get_next_line/get_next_line.c get_next_line/get_next_line_utils.c && ./a.out
 #gcc -Wall -Wextra -Werror -g -D BUFFER_SIZE=10000000 main.c get_next_line/get_next_line.c get_next_line/get_next_line_utils.c && ./a.out
-clang -Wall -Wextra -Werror -g -D BUFFER_SIZE=10000000 main.c get_next_line/get_next_line.c get_next_line/get_next_line_utils.c && ./a.out
+clang -Wall -Wextra -Werror -g -D BUFFER_SIZE=5 main.c get_next_line/get_next_line.c get_next_line/get_next_line_utils.c && ./a.out
 #valgrind --leak-check=full --tool=memcheck --show-leak-kinds=all --track-origins=yes get_next_line/get_next_line.c get_next_line/get_next_line_utils.c
